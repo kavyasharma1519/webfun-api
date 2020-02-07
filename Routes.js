00e3703193837021add2ce98c3e777da12c3576e
Sharma <sharmak@wwt.com>
2020-02-07 15:56:23 -0600

    WebDev

--- Routes.js
+++ Routes.js
@@ -4,15 +4,20 @@ module.exports = [
   {
     method: 'GET',
     path: '/',
+    options:{
     handler: Controller.sayHello,
     description: 'Hello World!',
     tags: ['api']
   }
+}
+
   ,
   {  method: 'GET',
       path: '/items',
+      options: {
       handler: Controller.getItems,
       description: 'This should return a number of items.',
       tags: ['api']
   }
+}
 ]
